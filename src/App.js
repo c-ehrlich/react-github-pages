57195e1baa17bbb704e3cdf1905dadc4c242e5ab
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-13 17:49:46 +0100

    implement useContext for accessing state in components

--- src/App.js
+++ src/App.js
@@ -1,83 +1,31 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-// we're using MUI 4.x which is outdated
-// also the Roboto font import neeeds to be in public/index.html
-// also we're using CssBaseline in index.js!
-import { Button } from '@material-ui/core';
+import { CssBaseline } from "@material-ui/core";
 
-const PokemonRow = ({ pokemon, onSelect }) => (
-  <tr>
-    <td>{pokemon.name.english}</td>
-    <td>{pokemon.type.join(", ")}</td>
-    <td>
-      <Button variant="contained" color="primary" onClick={() => onSelect(pokemon)}>Select!</Button>
-    </td>
-  </tr>
-);
+import PokemonInfo from "./components/PokemonInfo";
+import PokemonFilter from "./components/PokemonFilter";
+import PokemonTable from "./components/PokemonTable";
+import PokemonContext from "./PokemonContext";
 
-PokemonRow.propTypes = {
-  pokemon: PropTypes.shape({
-    name: PropTypes.shape({
-      english: PropTypes.string.isRequired,
-    }),
-    type: PropTypes.arrayOf(PropTypes.string.isRequired),
-  }),
-  onSelect: PropTypes.func.isRequired,
-};
-
-const PokemonInfo = ({ name, base }) => (
-  <div>
-    <h1>{name.english}</h1>
-    <table>
-      {Object.keys(base).map((key) => (
-        <tr key={key}>
-          <td>{key}</td>
-          <td>{base[key]}</td>
-        </tr>
-      ))}
-    </table>
-  </div>
-);
-
-PokemonInfo.propTypes = {
-  name: PropTypes.shape({
-    english: PropTypes.string.isRequired,
-  }),
-  base: PropTypes.shape({
-    HP: PropTypes.number.isRequired,
-    Attack: PropTypes.number.isRequired,
-    Defense: PropTypes.number.isRequired,
-    "Sp. Attack": PropTypes.number.isRequired,
-    "Sp. Defense": PropTypes.number.isRequired,
-    Speed: PropTypes.number.isRequired,
-  }),
-};
-
-const Container = styled.div`
+const Title = styled.h1`
+  text-align: center;
+`;
+const PageContainer = styled.div`
   margin: auto;
   width: 800px;
   padding-top: 1rem;
-`;
-const Title = styled.h1`
-  text-align: center;
 `;
 const TwoColumnLayout = styled.div`
   display: grid;
   grid-template-columns: 70% 30%;
   grid-column-gap: 1rem;
 `;
-const Input = styled.input`
-  width: 100%;
-  font-size: x-large;
-  padding: 0.2rem;
-`;
 
 function App() {
   const [filter, filterSet] = useState("");
-  const [pokemon, pokemonSet] = useState([]);
-  const [selectedItem, selectedItemSet] = useState(null);
+  const [pokemon, pokemonSet] = useState(null);
+  const [selectedPokemon, selectedPokemonSet] = useState(null);
 
   useEffect(() => {
     fetch("/react-github-pages/pokemon.json")
@@ -85,45 +33,37 @@ function App() {
       .then((data) => pokemonSet(data));
   }, []);
 
+  if (!pokemon) {
+    return <div>Loading data</div>;
+  }
+
   return (
-    <Container>
-      <Title>Pokemon Search</Title>
-      <TwoColumnLayout>
-        <div>
-          <Input
-            value={filter}
-            onChange={(event) => filterSet(event.target.value)}
-          />
-          <table width="100%">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {pokemon
-                .filter((pokemon) =>
-                  pokemon.name.english
-                    .toLowerCase()
-                    .includes(filter.toLowerCase())
-                )
-                .slice(0, 20)
-                .map((pokemon) => (
-                  <PokemonRow
-                    pokemon={pokemon}
-                    key={pokemon.id}
-                    onSelect={(pokemon) => selectedItemSet(pokemon)}
-                  />
-                ))}
-            </tbody>
-          </table>
-        </div>
-        {selectedItem && (
-          <PokemonInfo {...selectedItem} /> // spread operator!
-        )}
-      </TwoColumnLayout>
-    </Container>
+    <PokemonContext.Provider
+      value={{
+        filter,
+        pokemon,
+        selectedPokemon,
+        filterSet,
+        pokemonSet,
+        selectedPokemonSet
+      }}
+    >
+      <PageContainer>
+        <CssBaseline />
+        <Title>Pokemon Search</Title>
+        <TwoColumnLayout>
+          <div>
+            <PokemonFilter />
+            <PokemonTable
+              filter={filter}
+              pokemon={pokemon}
+              selectedPokemonSet={selectedPokemonSet}
+            />
+          </div>
+          <PokemonInfo {...selectedPokemon} />
+        </TwoColumnLayout>
+      </PageContainer>
+    </PokemonContext.Provider>
   );
 }
 
---
+++ src/components/PokemonInfo.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import PokemonContext from "../PokemonContext";
+import PokemonType from "../PokemonType";
+
+const PokemonInfo = () => {
+  const { selectedPokemon } = useContext(PokemonContext);
+
+  return selectedPokemon ? (
+    <div>
+      <h1>{selectedPokemon.name.english}</h1>
+      <table>
+        {Object.keys(selectedPokemon.base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{selectedPokemon.base[key]}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  ) : (
+    null
+  );
+};
+
+PokemonInfo.propTypes = PokemonType;
+
+export default PokemonInfo;
