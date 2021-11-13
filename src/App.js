3441a15d1c30d5fcbf563fcc7cd3dc54f49092ab
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-13 14:01:03 +0100

    CSS in JS using emotion (similar to styled components)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import PropTypes from "prop-types";
+import styled from "@emotion/styled";
 
 const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
@@ -50,6 +51,25 @@ PokemonInfo.propTypes = {
   }),
 };
 
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+const Title = styled.h1`
+  text-align: center;
+`;
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
 function App() {
   const [filter, filterSet] = useState("");
   const [pokemon, pokemonSet] = useState([]);
@@ -62,23 +82,11 @@ function App() {
   }, []);
 
   return (
-    <div
-      style={{
-        margin: "auto",
-        width: 800,
-        paddingTop: "1rem",
-      }}
-    >
-      <h1 className="title">Pokemon Search</h1>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "70% 30%",
-          gridColumnGap: "1rem",
-        }}
-      >
+    <Container>
+      <Title>Pokemon Search</Title>
+      <TwoColumnLayout>
         <div>
-          <input
+          <Input
             value={filter}
             onChange={(event) => filterSet(event.target.value)}
           />
@@ -110,8 +118,8 @@ function App() {
         {selectedItem && (
           <PokemonInfo {...selectedItem} /> // spread operator!
         )}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
